1d595e36adfff18fc2ffd2467ca68c3f18557c5f
Gitefy <lubbers_0betting@icloud.com>
2026-09-09 06:55:50 +0800

    build: align Android native library for 16 KB pages

--- libcore/build.sh
+++ libcore/build.sh
@@ -18,7 +18,9 @@ export GOBIND=gobind-matsuri
 source ../buildScript/lib/core/get_source_env.sh
 SING_BOX_VERSION="${SING_BOX_VERSION:-1.15.0}"
 SING_BOX_REVISION="${COMMIT_SING_BOX:-unknown}"
-LDFLAGS="-s -w -X github.com/sagernet/sing-box/constant.Version=${SING_BOX_VERSION} -X libcore.buildRevision=${SING_BOX_REVISION}"
+# Android 16 devices may use 16 KB pages. The APK is zip-aligned separately,
+# but the native ELF must also align every PT_LOAD segment to 16 KB.
+LDFLAGS="-s -w -linkmode=external -extldflags=-Wl,-z,max-page-size=16384 -X github.com/sagernet/sing-box/constant.Version=${SING_BOX_VERSION} -X libcore.buildRevision=${SING_BOX_REVISION}"
 "$GOPATH"/bin/gomobile-matsuri bind -v -androidapi 21 -cache "$(realpath $BUILD)" -trimpath -ldflags="$LDFLAGS" -tags='with_gvisor,with_quic,with_wireguard,with_utls,with_clash_api' . || exit 1
 rm -r libcore-sources.jar
 
